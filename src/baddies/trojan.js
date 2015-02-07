1e3dbef769c6e326aec5068c511cf392f5b28404
maxerdwien <max@erdwien.com>
2015-02-07 15:28:12 -0600

    added new files

---
+++ src/baddies/trojan.js
@@ -0,0 +1,95 @@
+var Trojan = function(x, y, path) {
+	this.x = x;
+	this.y = y;
+
+	this.path = path;
+	this.path_index = 0;
+	
+	this.image = resources.enemies_sprite_sheet;
+	this.spritex = 0;
+	this.spritey = 128;
+	
+	this.dead = false;
+	
+	this.attack = 5;
+	
+	this.right_foot = true;
+	
+	this.damage_level = 0;
+	
+	// milliseconds to travel one large pixel
+	this.millis_per_pixel = 65;
+	
+	this.millis_saved = 0;
+	this.pixels_traveled = 0;
+	
+	this.max_health = 300;
+	this.health = this.max_health;
+	
+	// milliseconds
+	this.max_sprite_cooldown = 700;
+	this.sprite_cooldown = this.max_sprite_cooldown;
+}
+
+Trojan.prototype = new Baddie();
+
+Trojan.prototype.update = function(elapsedTime) {
+	// move
+	this.millis_saved += elapsedTime;
+	if (this.millis_saved >= this.millis_per_pixel) {
+		this.millis_saved -= this.millis_per_pixel;
+		this.pixels_traveled++;
+		this.walk_direction = this.path[this.path_index];
+		switch (this.path[this.path_index]) {
+			case 0:
+				this.x += 4;
+				break;
+			case 1:
+				this.y -= 4;
+				break;
+			case 2:
+				this.x -= 4;
+				break;
+			case 3:
+				this.y += 4;
+				break;
+		}
+		if (this.pixels_traveled >= 16) {
+			this.path_index++;
+			if (this.path_index > this.path.length-1) {
+				this.attackCPU();
+			}
+			this.pixels_traveled = 0;
+		}
+	}
+	
+	// change animation frame
+	this.sprite_cooldown -= elapsedTime;
+	if (this.sprite_cooldown <= 0) {
+		this.sprite_cooldown = this.max_sprite_cooldown;
+		this.right_foot = !this.right_foot;
+	}
+}
+
+Trojan.prototype.render = function(ctx) {
+	ctx.save();
+	this.spritex = 64*this.damage_level + 384*this.right_foot;
+	if (this.walk_direction == 0 || this.walk_direction == 2) {
+		this.spritex += 768;
+	}
+	ctx.drawImage(this.image, this.spritex, this.spritey, 64, 64,
+		this.x, this.y, 64, 64);
+	ctx.restore();
+}
+
+Trojan.prototype.hurt = function(damage) {
+	this.health -= damage;
+	
+	// change sprite damage amount
+	if (this.health < (5-this.damage_level)*this.max_health/6) {
+		this.damage_level++;
+	}
+	if (this.health <= 0) {
+		this.dead = true;
+	}
+}
---
+++ src/towers/zappy_tower.js
@@ -0,0 +1,104 @@
+var Zappy_tower = function(x, y) {
+	this.x = x;
+	this.y = y;
+	
+	this.image = resources.towers_sprite_sheet;
+	this.spritex = 640;
+	this.spritey = 0;
+	
+	// damage per millisecond
+	this.damage = 0.1;
+	
+	this.max_cannon_cooldown = 2700;
+	this.cannon_cooldown = this.max_cannon_cooldown;
+	this.cannon_ready = true;
+	
+	this.max_firing_cooldown = 800;
+	this.firing_cooldown = this.max_firing_cooldown;
+	this.firing = false;
+	
+	this.mode = "ready";
+	
+	this.zap_index = 0;
+	this.max_zap_sprite_cooldown = 30;
+	this.zap_sprite_cooldown = this.max_zap_sprite_cooldown;
+}
+
+Zappy_tower.prototype = new Tower();
+
+Zappy_tower.prototype.update = function(elapsedTime) {
+	if (this.mode == "deployed") {
+		if (!this.cannon_ready) {
+			this.cannon_cooldown -= elapsedTime;
+			if (this.cannon_cooldown <= 0) {
+				this.cannon_ready = true;
+				this.cannon_cooldown = this.max_cannon_cooldown;
+			}
+		}
+		if (this.firing) {
+			this.firing_cooldown -= elapsedTime;
+			if (this.firing_cooldown <= 0) {
+				this.firing_cooldown = this.max_firing_cooldown;
+				this.firing = false;
+			}
+		}
+		
+		if (this.cannon_ready) {
+			for (var i = 0; i < game.baddies.length; i++) {
+				if (game.cd.detect(this.getRange(), game.baddies[i].getHitbox())) {
+					this.cannon_ready = false;
+					this.firing = true;
+					break;
+				}
+			}
+		}
+		
+		if (this.firing) {
+			for (var i = 0; i < game.baddies.length; i++) {
+				if (game.cd.detect(this.getRange(), game.baddies[i].getHitbox())) {
+					game.baddies[i].hurt(this.damage*elapsedTime);
+				}
+			}
+			this.zap_sprite_cooldown -= elapsedTime;
+			if (this.zap_sprite_cooldown <= 0) {
+				this.zap_sprite_cooldown = this.max_zap_sprite_cooldown;
+				this.zap_index++;
+				this.zap_index = this.zap_index % 3;
+			}
+		}
+	}
+}
+
+Zappy_tower.prototype.render = function(ctx) {
+	this.spritex = 640;
+	if (this.firing) {
+		this.spritex += 64;
+	}
+	ctx.save();
+	
+	ctx.drawImage(this.image, this.spritex, this.spritey, 64, 64,
+		this.x, this.y, 64, 64);
+	if (this.firing) {
+		ctx.drawImage(resources.zappy_sprite_sheet, 0+this.zap_index*192, 0, 192, 192, this.x+(32-96), this.y+(32-96), 192, 192);
+	}
+	ctx.restore();	
+}
+
+Zappy_tower.prototype.getRange = function() {
+	return {
+		type: "circle",
+		x: this.x + 32,
+		y: this.y + 32,
+		r: 96
+	};
+}
+
+Zappy_tower.prototype.getHitbox = function() {
+	return {
+		type: "rect",
+		x: this.x,
+		y: this.y,
+		w: 64,
+		h: 64
+	};
+}

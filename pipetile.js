365b76b43a43557ef2971f05e55c59d207951727
TheThirdWave <TheThirdWave@ksu.edu>
2015-02-06 23:06:10 -0600

    updated PipeTile

--- pipetile.js
+++ pipetile.js
@@ -1,8 +1,21 @@
-var pipeTile = function(x, y, game)
+var pipeTile = function(x, y, width, height, game)
 {
-
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.game = game;
 }
 
 pipeTile.prototype = {
 
+	render: function(context)
+	{
+		
+	}
+	
+	update: function()
+	{
+		
+	}
 }
